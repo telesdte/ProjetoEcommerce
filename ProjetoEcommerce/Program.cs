fa38ed60c85e0141dc2e16e00a642f33fa940a6f
telesdte <ltelesduarte@gmail.com>
2025-11-09 01:28:36 -0300

    8 - Criando as views e configurando a injeção de dependência no Program.cs

--- ProjetoEcommerce/Program.cs
+++ ProjetoEcommerce/Program.cs
@@ -1,10 +1,18 @@
+using ProjetoEcommerce.Repositorio;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
 
+// INJEÇÃO DE DEPENDENCIA 
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSession();
+builder.Services.AddScoped<UsuarioRepositorio>();
+builder.Services.AddScoped<ProdutoRepositorio>();
+
+var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
